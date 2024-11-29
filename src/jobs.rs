a708301c3305fa7baa2f8f91ed22f0a4d830761e
Drew Stone <drewstone329@gmail.com>
2024-11-29 15:21:07 -0700

    feat: add contract and get compiling

---
+++ src/jobs.rs
@@ -0,0 +1,118 @@
+use api::services::events::JobCalled;
+use ark_bn254::Bn254;
+use gadget_sdk::compute_sha256_hash;
+use gadget_sdk::network::round_based_compat::NetworkDeliveryWrapper;
+use gadget_sdk::{self as sdk};
+use sdk::event_listener::tangle::{
+    jobs::{services_post_processor, services_pre_processor},
+    TangleEventListener,
+};
+use sdk::tangle_subxt::tangle_testnet_runtime::api;
+use silent_threshold_encryption::encryption::Ciphertext;
+use silent_threshold_encryption::setup::{AggregateKey, SecretKey};
+use sp_core::ecdsa::Public;
+use std::collections::BTreeMap;
+
+use crate::context::{ServiceContext, KEYPAIR_KEY};
+use crate::decrypt::{threshold_decrypt_protocol, DecryptError};
+use crate::setup::from_bytes;
+
+/// Decrypts a ciphertext using the threshold decryption protocol
+#[sdk::job(
+    id = 0,
+    params(ciphertext, threshold),
+    result(_),
+    event_listener(
+        listener = TangleEventListener::<ServiceContext, JobCalled>,
+        pre_processor = services_pre_processor,
+        post_processor = services_post_processor,
+    ),
+)]
+pub async fn decrypt_ciphertext(
+    ciphertext: Vec<u8>,
+    threshold: u16,
+    context: ServiceContext,
+) -> Result<Vec<u8>, DecryptError> {
+    let blueprint_id = context
+        .blueprint_id()
+        .map_err(|e| DecryptError::ContextError(e.to_string()))?;
+    let call_id = context
+        .current_call_id()
+        .await
+        .map_err(|e| DecryptError::ContextError(e.to_string()))?;
+
+    // Deserialize the ciphertext
+    let ciphertext: Ciphertext<Bn254> = from_bytes(&ciphertext);
+
+    // Get the keypair from storage
+    let keypair = context
+        .secret_key_store
+        .get(KEYPAIR_KEY)
+        .ok_or(DecryptError::ContextError("Keypair not found".to_string()))?;
+
+    // Deserialize the secret key
+    let secret_key: SecretKey<Bn254> = from_bytes(&keypair.secret_key);
+
+    // Setup party information
+    let (i, operators) = context
+        .get_party_index_and_operators()
+        .await
+        .map_err(|e| DecryptError::ContextError(e.to_string()))?;
+
+    let parties: BTreeMap<u16, Public> = operators
+        .into_iter()
+        .enumerate()
+        .map(|(j, (_, ecdsa))| (j as u16, ecdsa))
+        .collect();
+
+    let num_parties = parties.values().len();
+
+    let (meta_hash, deterministic_hash) =
+        compute_deterministic_hashes(num_parties as u16, blueprint_id, call_id);
+    let network = NetworkDeliveryWrapper::new(
+        context.network_backend.clone(),
+        i as u16,
+        deterministic_hash,
+        parties,
+    );
+
+    let party = round_based::party::MpcParty::connected(network);
+    // TODO: Implement the AggregateKey struct properly
+    let pk = vec![];
+    let agg_key = AggregateKey::<Bn254>::new(pk, &context.params);
+    // Run the decryption protocol
+    let decryption = threshold_decrypt_protocol(
+        party,
+        i as u16,
+        threshold as u16,
+        num_parties as u16,
+        secret_key,
+        ciphertext,
+        agg_key,
+        &context.params,
+    )
+    .await?;
+
+    // Serialize the decryption
+    let decryption = serde_json::to_vec(&decryption)
+        .map_err(|e| DecryptError::SerializationError(e.to_string()))?;
+
+    Ok(decryption)
+}
+
+pub const KEYGEN_SALT: &str = "silent_timelock_encryption";
+pub const META_SALT: &str = "silent";
+
+/// Helper function to compute deterministic hashes for the keygen process
+fn compute_deterministic_hashes(n: u16, blueprint_id: u64, call_id: u64) -> ([u8; 32], [u8; 32]) {
+    let meta_hash = compute_sha256_hash!(
+        n.to_be_bytes(),
+        blueprint_id.to_be_bytes(),
+        call_id.to_be_bytes(),
+        META_SALT
+    );
+
+    let deterministic_hash = compute_sha256_hash!(meta_hash.as_ref(), KEYGEN_SALT);
+
+    (meta_hash, deterministic_hash)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
+use alloy_primitives::{Address, Bytes};
 use ark_bn254::Bn254;
 use ark_ec::pairing::Pairing;
 use ark_poly::univariate::DensePolynomial;
 use ark_std::UniformRand;
 use color_eyre::Result;
+use gadget_sdk::tangle_subxt::tangle_testnet_runtime::api::runtime_types::tangle_primitives::services::{BlueprintManager, ServiceBlueprint};
 use gadget_sdk as sdk;
+use gadget_sdk::config::StdGadgetConfiguration;
+use gadget_sdk::ext::tangle_subxt::tangle_testnet_runtime::api;
+use gadget_sdk::utils::evm::get_provider_http;
 use sdk::ctx::ServicesContext;
 use sdk::runners::tangle::TangleConfig;
 use sdk::runners::BlueprintRunner;
 use sdk::subxt_core::tx::signer::Signer;
 use silent_threshold_encryption::kzg::KZG10;
 use silent_timelock_encryption_blueprint::context::{ServiceContext, KEYPAIR_KEY};
-use silent_timelock_encryption_blueprint::setup::setup;
-use silent_timelock_encryption_blueprint::DecryptCiphertextEventHandler;
+use silent_timelock_encryption_blueprint::jobs::DecryptCiphertextEventHandler;
+use silent_timelock_encryption_blueprint::setup::{setup, SilentThresholdEncryptionKeypair};
+use silent_timelock_encryption_blueprint::SilentTimelockEncryptionBlueprint;
 
 #[sdk::main(env)]
 async fn main() -> Result<()> {
@@ -29,6 +35,7 @@ async fn main() -> Result<()> {
         _ => {
             let client = env.client().await?;
             let signer = env.first_sr25519_signer()?;
+            let service_id = env.service_id().unwrap();
             let operators = context.current_service_operators(&client).await?;
             let my_operator_position = operators
                 .iter()
@@ -37,6 +44,29 @@ async fn main() -> Result<()> {
             let new_keypair =
                 setup::<Bn254>(operators.len() as u32, my_operator_position as u32, params)
                     .expect("Failed to generate keypair");
+
+            // Submit the STE public key to the blueprint contract
+            let blueprint_id = context.blueprint_id()?;
+            let blueprint_storage_key = api::storage().services().blueprints(blueprint_id);
+            let blueprint: ServiceBlueprint = client
+                .storage()
+                .at_latest()
+                .await?
+                .fetch(&blueprint_storage_key)
+                .await
+                .map(|op| {
+                    if let Some(op) = op {
+                        op.1
+                    } else {
+                        panic!("Blueprint not found")
+                    }
+                })?;
+
+            let blueprint_contract_address = match blueprint.manager {
+                BlueprintManager::Evm(address) => Address::from(address.to_fixed_bytes()),
+            };
+            submit_ste_public_key(&env, service_id, &new_keypair, blueprint_contract_address)
+                .await?;
 
             context.secret_key_store.set(KEYPAIR_KEY, new_keypair);
         }
@@ -55,5 +85,34 @@ async fn main() -> Result<()> {
         .await?;
 
     tracing::info!("Exiting...");
+    Ok(())
+}
+
+/// Submit the STE public key to the blueprint contract for the given service ID
+async fn submit_ste_public_key(
+    env: &StdGadgetConfiguration,
+    service_id: u64,
+    keypair: &SilentThresholdEncryptionKeypair,
+    blueprint_contract_address: Address,
+) -> Result<()> {
+    let provider = get_provider_http(&env.http_rpc_endpoint);
+    let contract = SilentTimelockEncryptionBlueprint::new(blueprint_contract_address, provider);
+
+    // Submit the public key
+    contract
+        .registerSTEPublicKey(
+            service_id,
+            Bytes::copy_from_slice(keypair.public_key.as_ref()),
+        )
+        .send()
+        .await?
+        .get_receipt()
+        .await?;
+
+    tracing::info!(
+        "Successfully submitted STE public key for service ID {}",
+        service_id
+    );
+
     Ok(())
 }
